bb485551797a80e4326da250bdb7930d87e346e5
Rémy DELHAISE <rdelhaise@misyl-services.com>
2018-05-31 10:07:46 +0200

    Creation projet

---
+++ src/Basket/BasketTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasketTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
